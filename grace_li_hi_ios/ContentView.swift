cf7c82e44b955cb7fe72d779654cf06c5e55a84b
Tarun Natham <tarun.natham@berkeley.edu>
2025-07-04 16:15:52 -0700

    Initial Commit

---
+++ grace_li_hi_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  grace_li_hi_ios
+//
+//  Created by Tarun Natham 2 on 7/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ grace_li_hi_ios/grace_li_hi_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  grace_li_hi_iosApp.swift
+//  grace_li_hi_ios
+//
+//  Created by Tarun Natham 2 on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct grace_li_hi_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
